3f7ad1079adc2ba66c824228810951e3f8c43861
Funmilayo7 <77077406+Funmilayo7@users.noreply.github.com>
2021-01-07 07:15:47 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 69;
+            Console.WriteLine("The first number is " + firstNumber);
+
+            string Name = "";
+            Console.Write("Please what is your name: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Your name is " + Name);
+        
+        }
+    }
+}
